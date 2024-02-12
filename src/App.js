66d2ca719d708c73dcf8fbb7f6bc69823ac9ad51
protonio git-H <profiloh@mail.ru>
2024-02-12 23:01:57 +0300

    add toggle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
 
 
 function App() {
-  
+  const {onToggleButton,tg} = useTelegram();  
+
+  useEffect( () => {
+    tg.ready();
+  },[])
 
   return (
     <div className='App'>
-        work
+        <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
