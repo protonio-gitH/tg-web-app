031b160f92646e22c7df9c477a8f6120435c0590
protonio git-H <profiloh@mail.ru>
2024-02-13 17:48:01 +0300

    add hooks

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Form.css';
+import { useTelegram } from '../../hooks/useTelegram';
 
 const Form = () => {
     const [name , setName] = useState();
     const [problem , setProblem] = useState();
+    const {tg} = useTelegram();
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        })
+    }, [])
+
+    useEffect(() => {
+        if (!name || !problem){
+            tg.MainButton.hide();
+        }else{
+            tg.MainButton.show();
+        }
+    },[name,problem])
 
     const onChangeName = (e) => {
         setName(e.target.value);
@@ -18,7 +34,7 @@ const Form = () => {
             <div className={'container'}>
                 <form action="" className={'form'}>
                     <h2 className={'form__title'}>Проблема с товаром</h2>
-                    <input type="text" placeholder={'Название'} className={'form__input form__input-name'} value={name} onChange={onChangeName}/>
+                    <input type="text" placeholder={'Название товара'} className={'form__input form__input-name'} value={name} onChange={onChangeName}/>
                     
                     <textarea name="" id="" cols="30" rows="10" placeholder='Опишите проблему' className={'form__textarea'} value={problem} onChange={onChangeProblem}></textarea>
                 </form>
