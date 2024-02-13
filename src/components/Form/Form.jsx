b9d3e1a196a0ffa74929142677c5cfdbd3536298
protonio git-H <profiloh@mail.ru>
2024-02-13 19:10:38 +0300

    fix name,problem

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -13,14 +13,14 @@ const Form = () => {
             problem,
         }
         tg.sendData(JSON.stringify(data));
-    }, [])
+    }, [name,problem])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData);
         return () =>{
             tg.offEvent('mainButtonClicked', onSendData);
         }
-    }, [])
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
